a4a71480d91ca0ec06cf1d00a6f729c8233a8456
atonu <atonu.zahin@gmail.com>
2018-09-14 19:15:42 +0600

    db latest

--- SLF.sql
+++ SLF.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 14, 2018 at 11:48 AM
+-- Generation Time: Sep 14, 2018 at 03:15 PM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.8
 
@@ -65,54 +65,56 @@ CREATE TABLE `lost` (
   `contact` varchar(256) NOT NULL,
   `uname` varchar(50) NOT NULL,
   `date` varchar(255) NOT NULL,
-  `promotion` int(10) NOT NULL
+  `promotion` int(10) NOT NULL,
+  `reward` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lost`
 --
 
-INSERT INTO `lost` (`id`, `lost_name`, `age`, `image`, `image2`, `image3`, `last_located`, `origin`, `catagory`, `agent_name`, `contact`, `uname`, `date`, `promotion`) VALUES
-(26, 'Akkas Mia', 69, '/uploads/myImage (5).jpg', NULL, NULL, 'Kuratoli Road, BRA', 'Rajshahi', 'Lost', 'atonu', '017xxxxxxxx', '', '2018/07/31', 0),
-(28, 'Joshim Hawladar', 56, '/uploads/myImage (4).jpg', NULL, NULL, 'mirpur,Dhaka', 'Dhaka', 'Found', 'ahmed zahin', '123', '', '2018/07/31', 0),
-(31, 'shajib alam', 66, '/uploads/myImage (1).jpg', NULL, NULL, 'birulia bridge, Dhaka', 'Chittagong', 'Lost', 'Thakur', '018xxxxxxxx', '', '2018/07/31', 0),
-(32, 'Aunty', 80, '/uploads/myImage (2).jpg', NULL, NULL, 'Mogbazar,Dhaka', 'Jessore', 'Lost', 'Pulak Barmon', '019xxxxxxxx', '', '2018/07/31', 0),
-(33, 'tariqul Islam', 30, '/uploads/myImage-1536300178938.jpg', NULL, NULL, 'Kuratoli Road, BRA', 'No idea', 'Found', 'Nishe,Nishat', '017xxxxxxxx', '', '2018/07/31', 0),
-(35, 'lulu boyati', 65, '/uploads/pic (4).jpg', NULL, NULL, 'Shitakundo', 'Chittagong', 'found', 'ahmed zahin', '017xxxxxxxx', 'atonu', '2018/07/31', 0),
-(64, 'Kamal hossain', 56, '/uploads/pic (4).jpg', NULL, NULL, 'mirpur stadium premise', 'Dhaka', 'Lost', 'ahmed zahin', '017xxxxxxxx', 'atonu', '2018/08/01', 0),
-(65, 'shovic azmayn', 30, '/uploads/pic (3).jpg', NULL, NULL, 'basundhara', 'dinajpur', 'Lost', 'atonu', '019xxxx', 'atonu', '2018/08/01', 0),
-(66, 'wahid', 23, '/uploads/pic (5).jpg', NULL, NULL, 'mirpur,Dhaka', 'dhaka', 'Found', 'Pulak Barmon', '123', 'atonu', '2018/08/01', 0),
-(71, 'molla', 22, '/uploads/myImage-1536299779706.jpg', NULL, NULL, 'momahhadpur,Dhaka', 'Feni', 'Found', 'ahmed', '019', 'atonu', '2018/09/01', 0),
-(72, 'Chandler Bing', 40, '/uploads/myImage-1536300088129.jpg', NULL, NULL, 'Central Perk', 'Ireland', 'Lost', 'Monica', '234', 'atonu', '2018/09/01', 0),
-(73, 'promo', 20, '/uploads/myImage-1536298288841.jpg', NULL, NULL, 'jhigatola,Dhaka', 'Dhaka', 'Found', 'ahmed zahin', '123', 'atonu', '2018/09/01', 0),
-(74, 'promo - found', 12, '/uploads/myImage-1536299790639.jpg', NULL, NULL, 'shantinagar,Dhaka', 'Dhaka', 'Found', 'ahmed zahin', '123', 'atonu', '2018/09/01', 0),
-(75, 'Fatty', 12, '/uploads/myImage-1536298288841.jpg', NULL, NULL, 'jhigatola,Dhaka', 'Dhaka', 'found', 'ahmed zahin', '123', 'atonu', '2018/09/01', 240),
-(76, 'babu', 8, '/uploads/myImage (1).jpg', NULL, NULL, 'mirpur,Dhaka', 'No idea', 'lost', 'ahmed zahin', '123', 'atonu', '2018/09/02', 0),
-(77, 'abbas', 45, '/uploads/pic (2).jpg', NULL, NULL, 'kalshi,Dhaka', 'noakhali', 'lost', 'kashem', '017xxxxxx', 'atonu', '2018/09/03', 0),
-(78, 'admin post test', 44, '/uploads/myImage-1536299779706.jpg', NULL, NULL, 'Basundhara', 'not known', 'lost', 'ariya stark', '', '', '123', 0),
-(79, 'saddam', 54, '/uploads/myImage (3).jpg', NULL, NULL, 'shonarga', 'notor', 'found', 'kashem', '123xxx', 'atonu', '2018/09/03', 80),
-(80, 'Fatima', 52, '/uploads/pic (1).jpg', NULL, NULL, 'Kuratoli Road, BRA', 'Dhaka', 'found', 'ahmed zahin', '132', 'atonu', '2018/09/04', 80),
-(82, 'irfan', 123, '/uploads/pic (5).jpg', NULL, NULL, 'jhigatola,Dhaka', 'madaripur', 'lost', 'irfan', '33123123', 'atonu', '2018/09/05', 80),
-(83, 'just in case', 123, '/uploads/myImage-1536298288841.jpg', NULL, NULL, 'jhigatola,Dhaka', 'Dhaka', 'lost', 'ahmed zahin', '1332123', 'atonu', '2018/09/05', 0),
-(85, 'tarek', 1200, '/uploads/pic (2).jpg', NULL, NULL, 'jhigatola,Dhaka', 'Dhaka', 'lost', 'ahmed zahin', '123123', 'atonu', '2018/09/05', 80),
-(102, 'Ahmed', 23, '/uploads/pic (4).jpg', NULL, NULL, 'Dhaka', 'Dhaka', 'lost', 'Ahmed', '019xxxxxxxx', 'atonu', '2018/09/07', 80),
-(103, 'Xisan', 22, '/uploads/myImage-1536297566053.jpg', NULL, NULL, 'Wolf Pack HQ', 'Dhaka', 'lost', 'XYZ', '017xxxxxxxxx', 'atonu', '2018/09/07', 240),
-(104, 'Inan', 22, '/uploads/myImage-1536297967209.jpg', '/uploads/myImage-1536297566053.jpg', NULL, 'Wolf Pack HQ', 'Dhaka', 'found', 'Fatty', '017xxxxxxxx', 'atonu', '2018/09/07', 240),
-(105, 'Adib', 50, '/uploads/myImage-1536298093851.jpg', '/uploads/myImage-1536297566053.jpg', '/uploads/myImage-1536298288841.jpg', 'Wolf Pack HQ', 'No idea', 'lost', 'ahmed zahin', '123123123', 'atonu', '2018/09/07', 240),
-(106, 'Sobec', 23, '/uploads/myImage-1536298123458.jpg', NULL, NULL, 'Wolf Pack HQ', 'No idea', 'lost', 'ahmed zahin', '', 'atonu', '2018/09/07', 0),
-(107, 'Promo - lost', 23, '/uploads/myImage-1536299779706.jpg', NULL, NULL, 'Wolf Pack HQ', 'Dhaka', 'lost', 'WPfan69', '123123', 'atonu', '2018/09/07', 0),
-(108, 'admin post test', 22, '/uploads/myImage-1536339184196.jpg', NULL, NULL, 'can\'t tell', 'can\'t tell', 'lost', 'admin', '123', '', '123', 0),
-(109, 'admin image test', 12, '/uploads/myImage-1536339184196.jpg', NULL, NULL, 'jhigatola,Dhaka', 'Chittagong', 'found', 'mr. A', '019xxxxxxx', '', '019xxxxxxx', 0),
-(116, 'Nishee', 22, '/uploads/myImage-1536366618421.jpg', NULL, NULL, 'Shyamoli', 'Bagura', 'lost', 'ahmed zahin', '123xxxx', 'atonu', '2018/09/08', 80),
-(117, 'blnk test', 12, 'https://i.imgur.com/S58Jnn6.jpg', NULL, NULL, 'jhigatola,Dhaka', 'Dhaka', 'lost', 'ahmed zahin', '123123', 'atonu', '2018/09/08', 0),
-(118, 'lili', 17, '/uploads/myImage-1536368235818.jpg', NULL, NULL, 'Remakri', 'Bandorban', 'found', 'Shovic', '019xxxxxx', 'atonu', '2018/09/08', 0),
-(122, 'hridi', 19, '/uploads/myImage-1536510162644.jpg', NULL, NULL, 'UIU permanent campus', 'No idea', 'lost', 'Punk Inan', '019xxx', 'atonu', '2018/09/09', 0),
-(133, 'wahid', 23, '/uploads/pic (5).jpg', NULL, NULL, 'mirpur,Dhaka', 'dhaka', 'Found', 'Pulak Barmon', '019xxxx', 'atonu', '2018/09/10', 0),
-(134, 'Shovic', 23, '/uploads/myImage-1536298123458.jpg', NULL, NULL, 'Wolf Pack HQ', 'No idea', 'lost', 'ahmed zahin', '017xxxxxxxx', 'atonu', '2018/09/10', 0),
-(135, 'Promo - lost', 23, '/uploads/myImage-1536576872404.jpg', NULL, NULL, 'Wolf Pack HQ', 'Dhaka', 'lost', 'WPfan69', '123123', 'atonu', '2018/09/10', 0),
-(136, 'wahid', 23, '/uploads/pic (5).jpg', NULL, NULL, 'mirpur,Dhaka', 'dhaka', 'Found', 'Pulak Barmon', '016xxxx', 'atonu', '2018/09/10', 0),
-(302, 'wolf pack', 23, '/uploads/myImage-1536607929325.jpg', '/uploads/myImage2-1536607929327.jpg', '/uploads/myImage3-1536607929329.jpg', 'dhaka', 'Dhaka', 'lost', 'ahmed zahin', '017xxxxxxxx', 'atonu', '2018/09/11', 0),
-(307, 'demo', 22, '/uploads/myImage-1536625949447.jpg', '/uploads/myImage2-1536625949452.jpg', '/uploads/myImage3-1536625949453.jpg', 'jhigatola,Dhaka', 'dhaka', 'lost', 'ahmed zahin', '019xxx', 'atonu', '2018/09/11', 0);
+INSERT INTO `lost` (`id`, `lost_name`, `age`, `image`, `image2`, `image3`, `last_located`, `origin`, `catagory`, `agent_name`, `contact`, `uname`, `date`, `promotion`, `reward`) VALUES
+(26, 'Akkas Mia', 69, '/uploads/myImage (5).jpg', NULL, NULL, 'Kuratoli Road, BRA', 'Rajshahi', 'Lost', 'atonu', '017xxxxxxxx', '', '2018/07/31', 0, 0),
+(28, 'Joshim Hawladar', 56, '/uploads/myImage (4).jpg', NULL, NULL, 'mirpur,Dhaka', 'Dhaka', 'Found', 'ahmed zahin', '123', '', '2018/07/31', 0, 0),
+(31, 'shajib alam', 66, '/uploads/myImage (1).jpg', NULL, NULL, 'birulia bridge, Dhaka', 'Chittagong', 'Lost', 'Thakur', '018xxxxxxxx', '', '2018/07/31', 0, 0),
+(32, 'Aunty', 80, '/uploads/myImage (2).jpg', NULL, NULL, 'Mogbazar,Dhaka', 'Jessore', 'Lost', 'Pulak Barmon', '019xxxxxxxx', '', '2018/07/31', 0, 0),
+(33, 'tariqul Islam', 30, '/uploads/myImage-1536300178938.jpg', NULL, NULL, 'Kuratoli Road, BRA', 'No idea', 'Found', 'Nishe,Nishat', '017xxxxxxxx', '', '2018/07/31', 0, 0),
+(35, 'lulu boyati', 65, '/uploads/pic (4).jpg', NULL, NULL, 'Shitakundo', 'Chittagong', 'Found', 'ahmed zahin', '017xxxxxxxx', 'atonu', '2018/07/31', 0, 0),
+(64, 'Kamal hossain', 56, '/uploads/pic (4).jpg', NULL, NULL, 'mirpur stadium premise', 'Dhaka', 'Lost', 'ahmed zahin', '017xxxxxxxx', 'atonu', '2018/08/01', 0, 0),
+(65, 'shovic azmayn', 30, '/uploads/pic (3).jpg', NULL, NULL, 'basundhara', 'dinajpur', 'Lost', 'atonu', '019xxxx', 'atonu', '2018/08/01', 0, 0),
+(66, 'wahid', 23, '/uploads/pic (5).jpg', NULL, NULL, 'mirpur,Dhaka', 'dhaka', 'Found', 'Pulak Barmon', '123', 'atonu', '2018/08/01', 0, 0),
+(71, 'molla', 22, '/uploads/myImage-1536299779706.jpg', NULL, NULL, 'momahhadpur,Dhaka', 'Feni', 'Found', 'ahmed', '019', 'atonu', '2018/09/01', 0, 0),
+(72, 'Chandler Bing', 40, '/uploads/myImage-1536300088129.jpg', NULL, NULL, 'Central Perk', 'Ireland', 'Lost', 'Monica', '234', 'atonu', '2018/09/01', 0, 0),
+(73, 'promo', 20, '/uploads/myImage-1536298288841.jpg', NULL, NULL, 'jhigatola,Dhaka', 'Dhaka', 'Found', 'ahmed zahin', '123', 'atonu', '2018/09/01', 0, 0),
+(74, 'promo - found', 12, '/uploads/myImage-1536299790639.jpg', NULL, NULL, 'shantinagar,Dhaka', 'Dhaka', 'Found', 'ahmed zahin', '123', 'atonu', '2018/09/01', 0, 0),
+(75, 'Fatty', 12, '/uploads/myImage-1536298288841.jpg', NULL, NULL, 'jhigatola,Dhaka', 'Dhaka', 'Found', 'ahmed zahin', '123', 'atonu', '2018/09/01', 240, 0),
+(76, 'babu', 8, '/uploads/myImage (1).jpg', NULL, NULL, 'mirpur,Dhaka', 'No idea', 'Lost', 'ahmed zahin', '123', 'atonu', '2018/09/02', 0, 0),
+(77, 'abbas', 45, '/uploads/pic (2).jpg', NULL, NULL, 'kalshi,Dhaka', 'noakhali', 'Lost', 'kashem', '017xxxxxx', 'atonu', '2018/09/03', 0, 0),
+(78, 'admin post test', 44, '/uploads/myImage-1536299779706.jpg', NULL, NULL, 'Basundhara', 'not known', 'Lost', 'ariya stark', '', '', '123', 0, 0),
+(79, 'saddam', 54, '/uploads/myImage (3).jpg', NULL, NULL, 'shonarga', 'notor', 'Found', 'kashem', '123xxx', 'atonu', '2018/09/03', 80, 0),
+(80, 'Fatima', 52, '/uploads/pic (1).jpg', NULL, NULL, 'Kuratoli Road, BRA', 'Dhaka', 'Found', 'ahmed zahin', '132', 'atonu', '2018/09/04', 80, 0),
+(82, 'irfan', 123, '/uploads/pic (5).jpg', NULL, NULL, 'jhigatola,Dhaka', 'madaripur', 'Lost', 'irfan', '33123123', 'atonu', '2018/09/05', 80, 0),
+(83, 'just in case', 123, '/uploads/myImage-1536298288841.jpg', NULL, NULL, 'jhigatola,Dhaka', 'Dhaka', 'Lost', 'ahmed zahin', '1332123', 'atonu', '2018/09/05', 0, 0),
+(85, 'tarek', 1200, '/uploads/pic (2).jpg', NULL, NULL, 'jhigatola,Dhaka', 'Dhaka', 'Lost', 'ahmed zahin', '123123', 'atonu', '2018/09/05', 80, 1000),
+(102, 'Ahmed', 23, '/uploads/pic (4).jpg', NULL, NULL, 'Dhaka', 'Dhaka', 'Lost', 'Ahmed', '019xxxxxxxx', 'atonu', '2018/09/07', 80, 0),
+(103, 'Xisan', 22, '/uploads/myImage-1536297566053.jpg', NULL, NULL, 'Wolf Pack HQ', 'Dhaka', 'Lost', 'XYZ', '017xxxxxxxxx', 'atonu', '2018/09/07', 240, 5000),
+(104, 'Inan', 22, '/uploads/myImage-1536297967209.jpg', '/uploads/myImage-1536297566053.jpg', NULL, 'Wolf Pack HQ', 'Dhaka', 'Found', 'Fatty', '017xxxxxxxx', 'atonu', '2018/09/07', 240, 0),
+(105, 'Adib', 50, '/uploads/myImage-1536298093851.jpg', '/uploads/myImage-1536297566053.jpg', '/uploads/myImage-1536298288841.jpg', 'Wolf Pack HQ', 'No idea', 'Lost', 'ahmed zahin', '123123123', 'atonu', '2018/09/07', 240, 10000),
+(106, 'Sobec', 23, '/uploads/myImage-1536298123458.jpg', NULL, NULL, 'Wolf Pack HQ', 'No idea', 'Lost', 'ahmed zahin', '', 'atonu', '2018/09/07', 0, 0),
+(107, 'Promo - lost', 23, '/uploads/myImage-1536299779706.jpg', NULL, NULL, 'Wolf Pack HQ', 'Dhaka', 'Lost', 'WPfan69', '123123', 'atonu', '2018/09/07', 0, 0),
+(108, 'admin post test', 22, '/uploads/myImage-1536339184196.jpg', NULL, NULL, 'can\'t tell', 'can\'t tell', 'Lost', 'admin', '123', '', '123', 0, 0),
+(109, 'admin image test', 12, '/uploads/myImage-1536339184196.jpg', NULL, NULL, 'jhigatola,Dhaka', 'Chittagong', 'Found', 'mr. A', '019xxxxxxx', '', '019xxxxxxx', 0, 0),
+(116, 'Nishee', 22, '/uploads/myImage-1536366618421.jpg', NULL, NULL, 'Shyamoli', 'Bagura', 'Lost', 'ahmed zahin', '123xxxx', 'atonu', '2018/09/08', 80, 6000),
+(117, 'blnk test', 12, 'https://i.imgur.com/S58Jnn6.jpg', NULL, NULL, 'jhigatola,Dhaka', 'Dhaka', 'Lost', 'ahmed zahin', '123123', 'atonu', '2018/09/08', 0, 0),
+(118, 'lili', 17, '/uploads/myImage-1536368235818.jpg', NULL, NULL, 'Remakri', 'Bandorban', 'Found', 'Shovic', '019xxxxxx', 'atonu', '2018/09/08', 0, 0),
+(122, 'hridi', 19, '/uploads/myImage-1536928452606.jpg', '/uploads/myImage2-1536928452607.jpg', NULL, 'UIU permanent campus', 'No idea', 'Lost', 'Punk Inan', '019xxx', 'atonu', '2018/09/09', 0, 0),
+(133, 'wahid', 23, '/uploads/pic (5).jpg', NULL, NULL, 'mirpur,Dhaka', 'dhaka', 'Found', 'Pulak Barmon', '019xxxx', 'atonu', '2018/09/10', 0, 0),
+(134, 'Shovic', 23, '/uploads/myImage-1536298123458.jpg', NULL, NULL, 'Wolf Pack HQ', 'No idea', 'Lost', 'ahmed zahin', '017xxxxxxxx', 'atonu', '2018/09/10', 0, 0),
+(135, 'Promo - lost', 23, '', NULL, NULL, 'Wolf Pack HQ', 'Dhaka', 'Lost', 'WPfan69', '123123', 'atonu', '2018/09/10', 0, 0),
+(136, 'wahid', 23, '/uploads/pic (5).jpg', NULL, NULL, 'mirpur,Dhaka', 'dhaka', 'Found', 'Pulak Barmon', '016xxxx', 'atonu', '2018/09/10', 0, 0),
+(302, 'wolf pack', 30, '/uploads/myImage-1536927464196.jpg', '/uploads/myImage2-1536927464196.jpg', '/uploads/myImage3-1536927464198.jpg', 'dhaka', 'Dhaka', 'Lost', 'ahmed zahin', '017xxxxxxxx', 'atonu', '2018/09/11', 0, 0),
+(307, 'demo', 22, '/uploads/myImage-1536625949447.jpg', '/uploads/myImage2-1536625949452.jpg', '/uploads/myImage3-1536625949453.jpg', 'jhigatola,Dhaka', 'dhaka', 'Lost', 'ahmed zahin', '019xxx', 'atonu', '2018/09/11', 0, 0),
+(308, 'Lost Kid', 10, '/uploads/myImage-1536929032222.jpg', '/uploads/myImage2-1536929032224.jpg', NULL, 'Shewra', 'Unknown', 'lost', 'ahmed zahin', '123', 'atonu', '2018/09/14', 0, 1000);
 
 -- --------------------------------------------------------
 
@@ -214,7 +216,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `lost`
 --
 ALTER TABLE `lost`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=308;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=309;
 
 --
 -- AUTO_INCREMENT for table `records`
