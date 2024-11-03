bc52e71f58648be7a7d573d8f8b714f3b3a5aa72
ahmed zahin <atonu.zahin@gmail.com>
2024-11-04 05:20:40 +0600

    admin login solved

--- models/adminlogin-model.js
+++ models/adminlogin-model.js
@@ -9,7 +9,7 @@ var loginData = function(data, callback) {
     if (result.rows?.length == 0 || result == null) {
       callback(false);
     } else {
-      if (passwordHash.verify(data.password, result[0].password)) {
+      if (passwordHash.verify(data.password, result.rows[0].password)) {
         callback(result.rows);
       } else {
         callback(false);
